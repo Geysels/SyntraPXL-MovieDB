ab719cc1b4a9895714d7eb055385d2e02c1bdcb1
Geysels <nick@geysels.com>
2022-05-30 21:35:08 +0200

    adding uuid to user and creating favorites table

--- database/migrations/2022_05_30_171610_create_favorites_table.php
+++ database/migrations/2022_05_30_171610_create_favorites_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('movie_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
